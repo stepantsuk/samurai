260e90cfcde57dabb51975da1e8d4324ce0d237c
stepantsuk <stepan.tsukanov@yandex.ru>
2022-01-24 13:19:27 +0300

    lesson63 without followers

---
+++ src/api/api.js
@@ -0,0 +1,38 @@
+import * as axios from "axios";
+// import { follow, unfollow } from './../redux/usersReducer';
+
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    "API-KEY": '15aee2ab-2811-4194-9d22-fd4b67f44b29',
+  },
+});
+
+export const getUsers = (currentPage, pageSize) => {
+  return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+    return response.data;
+  })
+};
+
+
+/*
+export const unFollowUser = (id) => {
+  return instance.delete(`follow/${id}`).then(response => {
+    if (response.data.resultCode === 0) {
+      unfollow(id);
+    }
+  })
+};
+
+export const followUser = (id) => {
+  return instance.post(`follow/${id}`,{},).then(response => {
+    if (response.data.resultCode === 0) {
+      follow(id);
+    }
+  })
+};
+*/
+
+
