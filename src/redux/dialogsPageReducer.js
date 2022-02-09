fd2f665b53e694b1374b8301aca242a16d085a56
stepantsuk <stepan.tsukanov@yandex.ru>
2022-02-09 13:52:01 +0300

    dhfsdf

--- src/redux/dialogsPageReducer.js
+++ src/redux/dialogsPageReducer.js
@@ -13,49 +13,28 @@ let initialState = {
 };
 
 export const dialogsPageReducer = (state = initialState, action) => {
-
-  let stateCopy;
-
-  let _addMessage = () => {
-    let messageBody = { id: 4, message: state.newMessageText };
-    stateCopy = {
-      ...state,
-      messageData: [...state.messageData, messageBody],
-      newMessageText: '',
-    };
-  };
-
-  let _handleTextMessage = (messageValue) => {
-    stateCopy = {
-      ...state,
-      newMessageText: messageValue,
-    };
-  };
-
   switch (action.type) {
-    case 'ADD-MESSAGE':
-      _addMessage();
-      return stateCopy;
-    case 'HANDLE-TEXT-MESSAGE':
-      _handleTextMessage(action.messageText);
-      return stateCopy;
+    case 'ADD-MESSAGE': {
+      let messageBody = { id: 4, message: state.newMessageText };
+      state.messageData = [...state.messageData, messageBody];
+      state.newMessageText = '';
+      return { ...state };
+    }
+    case 'HANDLE-TEXT-MESSAGE': {
+      return {
+        ...state,
+        newMessageText: action.messageText
+      }
+    }
     default: return state;
   };
-
-  /*
-  if (action.type === 'ADD-MESSAGE') {
-    _addMessage();
-  } else if (action.type === 'HANDLE-TEXT-MESSAGE') {
-    _handleTextMessage(action.messageText);
-  };
-  */
 };
-
 export const createActionAddMsg = () => {
   return {
     type: 'ADD-MESSAGE'
   }
 };
+
 
 export const createActionHandleTextMsg = (text) => {
   return {
