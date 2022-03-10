0d07fdd26f4668576da5698b38450261f2cd75dd
stepantsuk <stepan.tsukanov@yandex.ru>
2022-03-10 10:00:06 +0300

    y

--- src/components/Profile/Post/Wall.jsx
+++ src/components/Profile/Post/Wall.jsx
@@ -5,7 +5,7 @@ const Wall = (props) => {
   return (
     <div className={wall.postContainer}>
       <div className={wall.postAvatar}>
-        <img src='https://b.radikal.ru/b28/1809/9f/ed08ae1127aa.jpg' className={wall.postAvatarImg}>
+        <img src='http://pscd.ru/uploads/posts/2015-01/1420715573_shreder.png' className={wall.postAvatarImg}>
         </img>
       </div>
       <div className={wall.postText}>
